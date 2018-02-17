68e174635c913a649f998d51a732353e0ffd9809
Alexei Dudarev <alexeidudarev@alexeis-mbp>
2018-02-17 09:51:43 +0200

    Category view controller added core data relation ship map added category view controller file added

---
+++ Todo/Controler/CategoryViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CategoryViewController.swift
+//  Todo
+//
+//  Created by Alexei Dudarev on 17/02/2018.
+//  Copyright © 2018 Alexei Dudarev. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    //MARK: - Table view datasource methods
+    
+    //MARK: - Table view delegate methods
+    
+    //MARK: - Data manipulation methods
+    
+    //MARK: - Add new categories
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+
+}
--- Todo/Controler/TodoListViewController.swift
+++ Todo/Controler/TodoListViewController.swift
@@ -171,8 +171,14 @@ extension TodoListViewController : UISearchBarDelegate{
 
     }
     
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        loadItems()
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0{
+            loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+        }
     }
 }
 
